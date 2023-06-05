752c5052105bf37576214127d902ea450929bbfa
Ebenezer Okello <okelloebenezer403@gmail.com>
2023-06-05 11:22:54 +0300

    hello

--- routes/Quiz.js
+++ routes/Quiz.js
@@ -103,9 +103,9 @@ router.put("/status/:id", async (req, res) => {
 
 router.put("/:id", async (req, res) => {
   const schema = Joi.object({
-    task: Joi.string().min(1).max(300),
+    task: Joi.string().min().max(300).required(),
     isComplete: Joi.boolean(),
-   
+    date: Joi.date(),
   });
 
   const { error } = schema.validate(req.body);
