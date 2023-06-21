e9ce9cc8f651dd7198484ddc394fe2600ab73a59
Ebenezer Okello <okelloebenezer403@gmail.com>
2023-06-21 13:42:04 +0300

    d

--- models/products.js
+++ models/products.js
@@ -1,70 +1,55 @@
 import mongoose from "mongoose";
-
-const tourSchema = mongoose.Schema(
+const reviewSchema = new mongoose.Schema(
   {
-    title: {
-      type: String,
-    },
-    description: {
-      type: String,
-    },
-    discountPrice: { type: Number },
-    brand: { type: String },
-    price: {
-      type: String,
-    },
-    picture: { type: String },
-    discountPercentage: {
-      type: Number,
-      min: [1, "wrong min discount"],
-      max: [99, "wrong max discount"],
-    },
-    location: { type: String },
-    numberItems: { type: Number },
-    age: { type: Number },
-    infoAdd: { type: String },
-    specifications: { type: String },
-    category: {
-      type: String,
-    },
-    rating: {
-      type: Number,
-      min: [0, "wrong min rating"],
-      max: [5, "wrong max price"],
-      default: 0,
-    },
-    stock: { type: Number, min: [0, "wrong min stock"], default: 0 },
-    images: {
-      type: Array,
-    },
-
-    address: { type: String },
-
-    picture: { type: String },
-
-    userId: {
-      type: String,
-      required: true,
-    },
-    isComplete: { type: Boolean, default: false },
-    countInStock: { type: Number },
-    task: { type: Number },
-    quiz: { type: String },
+    name: { type: String },
+    comment: { type: String },
     rating: { type: Number },
-    numReviews: { type: Number },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const tourSchema = mongoose.Schema({
+  name: {type:String},
+  
+  phone:{type:String},
+  address:{type:String},
+ 
+  userId: {
+    type: String,
+    // required: true,
+  },
+  picture: {type:String},
+  
+  status: {
+    type: String,
+    enum: ['pending', 'success', 'rejected'],
+    default: 'pending',
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  isComplete: {type:Boolean , default:false},
+  countInStock: { type: Number},
+task:{type:Number},
+quiz:{type:String},
+rating: { type: Number },
+    numReviews: { type: Number},
+    reviews: [reviewSchema],
     driverName: String,
     driverTell: String,
-    creator: String,
-    quiz: String,
-    feedback: String,
-    createdAt: {
-      type: Date,
-      default: new Date(),
-    },
-    date: { type: Date, default: new Date() },
+  creator: String,
+  quiz: String,
+  feedback: String,
+  createdAt: {
+    type: Date,
+    default: new Date(),
   },
-  { timestamps: true }
-);
+  date: { type: Date, default: new Date() },
+},
+{timestamps:true});
 
 const TourModal = mongoose.model("customer", tourSchema);
 
