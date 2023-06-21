542b30b6e76e407235077a59d623c95135af1fe6
Ebenezer Okello <okelloebenezer403@gmail.com>
2023-06-21 13:14:54 +0300

    hfhf

--- models/products.js
+++ models/products.js
@@ -1,91 +1,73 @@
 import mongoose from "mongoose";
 
-
-
-
-
-
-
-
-
-
-const tourSchema = mongoose.Schema({
- 
+const tourSchema = mongoose.Schema(
+  {
     name: {
       type: String,
-      required: [true, "can't be blank"]
+      required: [true, "can't be blank"],
     },
     description: {
       type: String,
-      required: [true, "can't be blank"]
     },
-    discountPrice: { type: Number},
-    brand: { type: String},
-  creator:{type:String},
+    discountPrice: { type: Number },
+    brand: { type: String },
     price: {
       type: String,
-      required: [true, "can't be blank"]
+      required: [true, "can't be blank"],
     },
-    picture:{type:String},
-    discountPercentage: { type: Number, min:[1, 'wrong min discount'], max:[99, 'wrong max discount']},
-    location:{type:String},
-    numberItems:{type:Number},
-    age:{type:Number},
-    infoAdd:{type:String},
-    specifications:{type:String},
+    picture: { type: String },
+    discountPercentage: {
+      type: Number,
+      min: [1, "wrong min discount"],
+      max: [99, "wrong max discount"],
+    },
+    location: { type: String },
+    numberItems: { type: Number },
+    age: { type: Number },
+    infoAdd: { type: String },
+    specifications: { type: String },
     category: {
       type: String,
-     
     },
-    rating: { type: Number, min:[0, 'wrong min rating'], max:[5, 'wrong max price'], default:0},
-  stock: { type: Number, min:[0, 'wrong min stock'], default:0},
+    rating: {
+      type: Number,
+      min: [0, "wrong min rating"],
+      max: [5, "wrong max price"],
+      default: 0,
+    },
+    stock: { type: Number, min: [0, "wrong min stock"], default: 0 },
     images: {
       type: Array,
-      
     },
- 
 
+    address: { type: String },
 
+    picture: { type: String },
 
-
-  
-
-  address:{type:String},
- 
-  userId: {
-    type: String,
-    // required: true,
-  },
-  picture: {type:String},
-  
-  status: {
-    type: String,
-    enum: ['pending', 'success', 'rejected'],
-    default: 'pending',
-  },
-  userId: {
-    type: String,
-    required: true,
-  },
-  isComplete: {type:Boolean , default:false},
-  countInStock: { type: Number},
-task:{type:Number},
-quiz:{type:String},
-rating: { type: Number },
-    numReviews: { type: Number},
+    userId: {
+      type: String,
+      required: true,
+    },
+    isComplete: { type: Boolean, default: false },
+    countInStock: { type: Number },
+    task: { type: Number },
+    quiz: { type: String },
+    rating: { type: Number },
+    numReviews: { type: Number },
     reviews: [reviewSchema],
     driverName: String,
     driverTell: String,
-  creator: String,
-  quiz: String,
-  feedback: String,
-  createdAt: {
-    type: Date,
-    default: new Date(),
+    creator: String,
+    quiz: String,
+    feedback: String,
+    createdAt: {
+      type: Date,
+      default: new Date(),
+    },
+    date: { type: Date, default: new Date() },
   },
-  date: { type: Date, default: new Date() },
-},
-{timestamps:true});
+  { timestamps: true }
+);
 
 const TourModal = mongoose.model("customer", tourSchema);
 
